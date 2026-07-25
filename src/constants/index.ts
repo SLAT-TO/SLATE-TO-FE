04c15388c0125009338970e04f2109c86afe89d4
kcleverp <kcleverp@gmail.com>
2026-07-25 15:01:21 +0900

    fix: mypage merge 후 누락된 videoCategories 복구 (#61)
    - origin/dev mypage가 참조하는 constants/videoCategories를 복원해 Vercel tsc 빌드 실패 해소
    - constants barrel에 projectLabels와 videoCategories export를 함께 유지

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,11 @@
-export { FEEDBACK_TYPE_LABELS, FEEDBACK_TYPE_OPTIONS, type FeedbackType } from './feedbackTypes'
+﻿export { FEEDBACK_TYPE_LABELS, FEEDBACK_TYPE_OPTIONS, type FeedbackType } from './feedbackTypes'
 export { ROLE_LABELS, ROLE_OPTIONS, type Role } from './roles'
 export { PROJECT_TYPE_LABEL, PROJECT_LENGTH_TYPE_LABEL, projectMetaTags } from './projectLabels'
+export {
+  FILM_LENGTH_LABELS,
+  FILM_LENGTH_OPTIONS,
+  VIDEO_CATEGORY_LABELS,
+  VIDEO_CATEGORY_OPTIONS,
+  type FilmLength,
+  type VideoCategory,
+} from './videoCategories'
