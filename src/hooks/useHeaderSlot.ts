fd9ca70b2f7420ef51345b4e47e70b5aa81121b3
Junghyun Seo <jseo0508@gmail.com>
2026-07-26 02:59:32 +0900

    docs: useHeaderSlot content 메모이제이션 주의사항 주석 추가 (#116)

--- src/hooks/useHeaderSlot.ts
+++ src/hooks/useHeaderSlot.ts
@@ -5,6 +5,9 @@ import { HeaderSlotContext } from '../layouts/headerSlotContext'
 /**
  * 페이지가 전역 헤더 왼쪽 영역을 채운다.
  * mount 시 설정, unmount 시 비움.
+ *
+ * 주의: content에 인라인 JSX를 넘기면 매 렌더 새 객체가 되어 effect가 반복 실행된다.
+ * 컴포넌트 밖 상수로 분리하거나 useMemo로 감쌀 것.
  */
 export function useHeaderSlot(content: ReactNode) {
   const context = useContext(HeaderSlotContext)
