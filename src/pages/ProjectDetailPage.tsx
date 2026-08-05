75b5deddebdab17f94297c8070188c9c7b56a244
kcleverp <kcleverp@gmail.com>
2026-08-06 06:18:07 +0900

    style: ProjectDetailPage Prettier 포맷 정리 (#246)

--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -46,10 +46,7 @@ type ProjectDetailPageProps = {
   videoId?: number | null
 }
 
-export default function ProjectDetailPage({
-  projectId,
-  videoId = null,
-}: ProjectDetailPageProps) {
+export default function ProjectDetailPage({ projectId, videoId = null }: ProjectDetailPageProps) {
   const {
     project,
     setProject,
