607cbfb39fe13d24df5c7bcaa9636f01dfe947a2
seoyeon ahn <ahnseoyn@gmail.com>
2026-08-10 01:24:36 +0900

    fix: 리뷰 반영

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -43,7 +43,7 @@ export default function HomePage() {
 
         <aside className="flex w-full shrink-0 flex-col items-center lg:w-81.75">
           <HomeMiniCalendar className="self-end" />
-          <div className="mt-11 -mr-1 w-61.5 self-end">
+          <div className="mt-11 w-61.5 self-end">
             <HomeTodayScheduleCard schedules={todaySchedules} loading={loading} />
           </div>
         </aside>
